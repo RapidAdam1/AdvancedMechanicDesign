6833345877d528d9d99b2facdd33394c8a497c99
RapidAdam1 <79019053+RapidAdam1@users.noreply.github.com>
2024-01-17 22:20:21 +0000

    WORKING

--- Assets/UniWork/01-SplineMeshGenerator/Scripts/SplineMesh.cs
+++ Assets/UniWork/01-SplineMeshGenerator/Scripts/SplineMesh.cs
@@ -18,7 +18,7 @@ public class SplineMesh : MonoBehaviour
 
 
     //TrackGeneration Variables
-    [SerializeField] [Range(3,500)]int Resolution;
+    [SerializeField] [Range(1,500)]int Resolution;
     [SerializeField] [Range(-4,4)]float m_Offset;
     [SerializeField] Vector2 Scale = Vector2.one;
 
@@ -62,13 +62,11 @@ public class SplineMesh : MonoBehaviour
             m_SplineContainer.Evaluate(m_Index, t, out Position, out Tangent, out UpVector);
             float3 right = Vector3.Cross(Tangent, UpVector).normalized;
             Vector3 Point = Position + (right * m_Offset);
-            BuildMesh(Point,m_Verts.Count);
+            BuildMesh(Point,i*step<Resolution);
         }
 
         if (!m_SplineContainer.Spline.Closed)
-        {
             BuildEnds(m_Verts.Count);
-        }
 
         m_Mesh.Clear();
         m_Mesh.SetVertices(m_Verts);
@@ -76,53 +74,73 @@ public class SplineMesh : MonoBehaviour
         m_Mesh.RecalculateNormals();
     }
 
-    private void BuildMesh(Vector3 SegmentPos,int RefTri)
+    private void BuildMesh(Vector3 SegmentPos,bool IsLast)
     {
         // Z Face
         Vector3 Right = Vector3.Cross(Tangent, UpVector).normalized;
         Vector3 Up = (Vector3)UpVector;
-        m_Verts.Add(SegmentPos + (-Right * Scale.x + Up * Scale.y)); //TL 0
-        m_Verts.Add(SegmentPos + (Right * Scale.x + Up * Scale.y)); //TR 1
-        m_Verts.Add(SegmentPos + (Right * Scale.x + -Up * Scale.y)); //BR 2
-        m_Verts.Add(SegmentPos + (-Right * Scale.x + -Up * Scale.y)); //BL 3
-        if(RefTri > 3)
+        m_Verts.Add(SegmentPos + (-Right * Scale.x + Up * Scale.y)); //TR 0
+        m_Verts.Add(SegmentPos + (Right * Scale.x + Up * Scale.y)); //TL 1
+        m_Verts.Add(SegmentPos + (Right * Scale.x + -Up * Scale.y)); //BL 2
+        m_Verts.Add(SegmentPos + (-Right * Scale.x + -Up * Scale.y)); //BR 3
+        int RefTri = m_Verts.Count -1;
+        Debug.Log(RefTri);
+        if(RefTri >= 7)
         {
-            //Top face 0 1 , 4 5
-            m_Tris.AddRange(new int[] {0, 1, 4 }); // Outer
-            m_Tris.AddRange(new int[] {4, 1, 5 }); // Inner
+            //Red   0 & 4
+            //Blue  1 & 5
+            //Yell  2 & 6
+            //Green 3 & 7
 
-            //Left Face 1 2 ,5 6
-            m_Tris.AddRange(new int[] {1, 2, 5 }); //Bottom
-            m_Tris.AddRange(new int[] {5, 2, 6 }); //Top
+            //Right Face 
+            m_Tris.AddRange(new int[] { RefTri - 7 , RefTri - 3, RefTri - 4 }); 
+            m_Tris.AddRange(new int[] { RefTri - 3, RefTri - 0 , RefTri - 4 }); 
+           
+            m_Tris.AddRange(new int[] { RefTri -2 , RefTri -3 , RefTri -6  }); 
+            m_Tris.AddRange(new int[] { RefTri -3 , RefTri -7 , RefTri -6  }); 
 
-            // Outer Face 0 3, 4 7
-            m_Tris.AddRange(new int[] {0, 4, 3 }); //Bottom
-            m_Tris.AddRange(new int[] {3, 4, 7 }); //Top
+            m_Tris.AddRange(new int[] { RefTri - 5, RefTri - 4 , RefTri - 1 }); 
+            m_Tris.AddRange(new int[] { RefTri - 4, RefTri - 0, RefTri - 1  });
 
-            //Bottom 2 3, 5 7
-            m_Tris.AddRange(new int[] { 6, 2, 3}); // Outer
-            m_Tris.AddRange(new int[] { 6, 3, 7 }); // Inner
+            m_Tris.AddRange(new int[] { RefTri - 2, RefTri - 6, RefTri - 1 });
+            m_Tris.AddRange(new int[] { RefTri -6 , RefTri -5 , RefTri - 1 });
+
+
         }
     }
 
     void BuildEnds(int Verts)
     {
+        Verts -= 1;
         //First
-        m_Tris.AddRange(new int[] { 3, 1, 0 });
-        m_Tris.AddRange(new int[] { 3, 2, 1 });
+        m_Tris.AddRange(new int[] { 1, 0, 2 });
+        m_Tris.AddRange(new int[] { 2, 0, 3 });
 
-        //Last
-        m_Tris.AddRange(new int[] { Verts -2,   Verts -4,   Verts-3 });
-        m_Tris.AddRange(new int[] { Verts -2,   Verts -1,   Verts-4 });
+/*        //Last
+        m_Tris.AddRange(new int[] { Verts -1,Verts -3,  Verts-2 });
+        m_Tris.AddRange(new int[] { Verts -1,Verts,     Verts-3 });*/
 
     }
 
     private void OnDrawGizmos()
     {
-/*        Handles.matrix = transform.localToWorldMatrix;
-        for (int i = 0; i < m_Verts.Count; i++)
-        {
-            Handles.SphereHandleCap(i, m_Verts[i], Quaternion.identity, 0.1f, EventType.Repaint);
-        }*/
+        Handles.matrix = transform.localToWorldMatrix;
+        Handles.color = Color.red;Handles.SphereHandleCap(0, m_Verts[0], Quaternion.identity, 0.1f, EventType.Repaint);
+        Handles.color = Color.blue;Handles.SphereHandleCap(0, m_Verts[1], Quaternion.identity, 0.1f, EventType.Repaint);
+        Handles.color = Color.yellow;Handles.SphereHandleCap(0, m_Verts[2], Quaternion.identity, 0.1f, EventType.Repaint);
+        Handles.color = Color.green;Handles.SphereHandleCap(0, m_Verts[3], Quaternion.identity, 0.1f, EventType.Repaint);
+
+        Handles.color = Color.red; Handles.SphereHandleCap(0, m_Verts[4], Quaternion.identity, 0.1f, EventType.Repaint);
+        Handles.color = Color.blue; Handles.SphereHandleCap(0, m_Verts[5], Quaternion.identity, 0.1f, EventType.Repaint);
+        Handles.color = Color.yellow;Handles.SphereHandleCap(0, m_Verts[6], Quaternion.identity, 0.1f, EventType.Repaint);
+        Handles.color = Color.green;Handles.SphereHandleCap(0, m_Verts[7], Quaternion.identity, 0.1f, EventType.Repaint);
+
+                for (int i = 1; i < m_Verts.Count-1; i++)
+                {
+                    Handles.SphereHandleCap(i, m_Verts[i], Quaternion.identity, 0.03f, EventType.Repaint);
+                }
+                Handles.color = Color.red;
+                Handles.SphereHandleCap(0, m_Verts[m_Verts.Count-1], Quaternion.identity, 0.1f, EventType.Repaint);
+        
     }
 }
