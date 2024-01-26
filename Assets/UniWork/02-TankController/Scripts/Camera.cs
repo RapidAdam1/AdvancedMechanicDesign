f192a6b126ced3a142ef6c91b7eac8b13a66bdab
RapidAdam1 <79019053+RapidAdam1@users.noreply.github.com>
2024-01-26 14:55:08 +0000

    Tank LOOK

--- Assets/UniWork/02-TankController/Scripts/Camera.cs
+++ Assets/UniWork/02-TankController/Scripts/Camera.cs
@@ -8,26 +8,30 @@ public class CameraController : MonoBehaviour
 	[SerializeField] private Transform m_CameraMount;
 	[SerializeField] private Camera m_Camera;
 
+	//Zoom && Collision
 	private float m_CameraDist = 5f;
+	[SerializeField] private float m_MaxDist;
+	[SerializeField] private float m_MinDist;
 
 	[SerializeField] private float m_YawSensitivity;
 	[SerializeField] private float m_PitchSensitivity;
 	[SerializeField] private float m_ZoomSensitivity;
 
-	[SerializeField] private float m_MaxDist;
-	[SerializeField] private float m_MinDist;
-
-	[SerializeField] private float m_CameraProbeSize;
-	[SerializeField] private Vector3 m_TargetOffset;
+	[SerializeField] private float m_CameraProbeSize; 
+	[SerializeField] private Vector3 m_TargetOffset; 
 
 	public void RotateSpringArm(Vector2 change)
 	{
+		m_TargetOffset.x += change.x * m_YawSensitivity;
+		m_TargetOffset.y -= change.y * m_PitchSensitivity;
 
+		m_SpringArmTarget.transform.rotation = Quaternion.Euler(m_TargetOffset.y, m_TargetOffset.x, 0);
 	}
 
 	public void ChangeCameraDistance(float amount)
 	{
-
+		m_CameraDist = Mathf.Clamp(m_CameraDist+amount,m_MinDist,m_MaxDist);
+		Debug.Log(m_CameraDist);
 	}
 
 	private void LateUpdate()
