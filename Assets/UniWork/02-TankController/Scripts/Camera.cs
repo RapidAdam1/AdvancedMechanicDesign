f225aa1b494311b2d451d4a05ef62491e60d5fb0
Adam Shrimpton <79019053+RapidAdam1@users.noreply.github.com>
2024-02-26 17:00:42 +0000

    Camera Occlusion Working

--- Assets/UniWork/02-TankController/Scripts/Camera.cs
+++ Assets/UniWork/02-TankController/Scripts/Camera.cs
@@ -43,22 +43,37 @@ public class CameraController : MonoBehaviour
 
 	private void LateUpdate()
 	{
+		m_SpringArmTarget.transform.position = Vector3.MoveTowards(m_SpringArmTarget.transform.position, this.transform.position, 1f);
+		
+		
 		float CameraDistance;
-		Vector3 RayStart = m_SpringArmTarget.position + CameraOffset;
+		Vector3 RayStart = transform.position + new Vector3(0,CameraOffset.y);
 		Vector3 Direction = m_CameraMount.transform.position - RayStart;
-
+		DebugLocationStart = RayStart;
 		bool IsCollidingWithGround = Physics.Raycast(RayStart,Direction, out RaycastHit HitInfo, m_CameraDist);
         if (IsCollidingWithGround)
         {
-			CameraDistance = HitInfo.distance;
+			DebugLocationHit = HitInfo.point;
+			CameraDistance = HitInfo.distance-5;
+			Debug.DrawRay(RayStart, Direction);
         }
         else
         {
 			CameraDistance = m_CameraDist;
         }
+	
 		m_CameraMount.localPosition = new Vector3(CameraOffset.x, CameraOffset.y, -CameraDistance);
 
-		m_SpringArmTarget.transform.position = Vector3.MoveTowards(m_SpringArmTarget.transform.position, this.transform.position, 1f);
 	}
 
+	Vector3 DebugLocationHit = Vector3.zero;
+	Vector3 DebugLocationStart = Vector3.zero;
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+		Gizmos.DrawSphere(DebugLocationStart, 0.2f);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawSphere(DebugLocationHit,0.5f);
+    }
 }
